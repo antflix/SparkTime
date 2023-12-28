9c30f182c6de1b98bc63d8e045b14eb1eb39c3ca
Anthony <anthonymeo88@gmail.com>
2023-12-28 01:56:38 -0600

    SparkTime Intigration

---
+++ SparkTime/components/BackButton.swift
@@ -0,0 +1,32 @@
+//
+//  BackButton.swift
+//  Time Turner-Inner 3.0
+//
+//  Created by User on 12/27/23.
+//
+
+import SwiftUI
+
+struct BackButton<Destination: View>: View {
+	let destination: Destination
+	@Binding var isActive: Bool
+	init(destination: Destination, isActive: Binding<Bool>) {
+		self.destination = destination
+		self._isActive = isActive
+	}
+
+	var body: some View {
+		
+		NavigationLink(destination: destination) {
+			
+				Image(systemName: "arrow.left")
+					.foregroundColor(.white)
+					.buttonStyle(PlainButtonStyle())
+
+			}
+
+		}
+
+	}
+
+
