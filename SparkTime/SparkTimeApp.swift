8187b21e7c527338969160b0b27dc2f213f907e4
Anthony <anthonymeo88@gmail.com>
2023-12-28 01:38:17 -0600

    Initial Commit

---
+++ SparkTime/SparkTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkTimeApp.swift
+//  SparkTime
+//
+//  Created by User on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SparkTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
